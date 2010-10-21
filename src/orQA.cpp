7642229907e27e2cdd7c22900d50466421123c22
Florian Klinglmueller <float@lefant.net>
2010-10-21 00:00:00 +0000

    version 0.2.1

--- src/orQA.cpp
+++ src/orQA.cpp
@@ -1,85 +1,76 @@
 #include <Rcpp.h>
-
 #include <functional>
+using namespace Rcpp ;
 
-extern "C" {
-void C_isomean(double* y, double* w, int* n, double* ghat)
-{
-  int c, j, nn; 
-  double neu;
-  double* gew;
-  int* k;
+void Rcpp_isomean(
+    const NumericMatrix::Row y , 
+    double* w, 
+    int nn,
+    int* k, 
+    double* gew, 
+    double* ghat
+){
   
-  nn = *n; /* nn > 1 */
- 
-  /* allocate vector - error handling is done by R */
-  k = (int *) Calloc((size_t) nn, int);
-  gew = (double *) Calloc((size_t) nn, double);
+    int c, j; 
+    double neu;
 
-  c = 0;
-  k[c] = 0;
-  gew[c] = w[0];
-  ghat[c] = y[0];
-
-  for (j=1; j < nn; j++)
-  {  
-    c = c+1;
-    k[c] = j;
-    gew[c] = w[j];
-    ghat[c] = y[j];
-
-    /* c is at least 1 as nn is > 1 */
-    while (ghat[c-1] >= ghat[c])
-    {
-      neu = gew[c]+gew[c-1];
-      ghat[c-1] = ghat[c-1]+(gew[c]/neu)*(ghat[c]-ghat[c-1]);
-      gew[c-1] = neu;
+    c = 0;
+    k[c] = 0;
+    gew[c] = w[0];
+    ghat[c] = y[0];
+    
+    for (j=1; j < nn; j++){  
+        c = c+1;
+        k[c] = j;
+        gew[c] = w[j];
+        ghat[c] = y[j];
+        
+        /* c is at least 1 as nn is > 1 */
+        while (ghat[c-1] >= ghat[c]){
+            neu = gew[c]+gew[c-1];
+            ghat[c-1] = ghat[c-1]+(gew[c]/neu)*(ghat[c]-ghat[c-1]);
+            gew[c-1] = neu;
+            c = c-1;
+            
+            if (c==0) break;
+        }
+    }
+    
+    while (nn >= 1){
+      for (j=k[c]; j < nn; j++){
+        ghat[j] = ghat[c];
+      }
+      nn = k[c];
       c = c-1;
-
-      if (c==0) break;
     }
-  }
+}
 
-  while (nn >= 1)
-  {
-    for (j=k[c]; j < nn; j++)
-    {
-      ghat[j] = ghat[c];
+// BEGIN_RCPP and END_RCPP bracket the code so that C++ exceptions are passed back to R
+extern "C" SEXP misoreg ( SEXP data, SEXP weights ) {
+BEGIN_RCPP
+
+    // we don't make modifications to those
+    NumericMatrix input(data);
+    NumericVector w(weights);
+    int n = input.nrow(), m = input.ncol();
+    
+    NumericMatrix output(n,m);
+    
+    // since these always have the same size, we just allocate them here once
+    IntegerVector k(m) ;
+    NumericVector gew(m), ghat(m) ;
+    
+    for(int i=0; i<n; i++){
+        Rcpp_isomean(
+            input.row(i), w.begin(), m, 
+            k.begin(), gew.begin(), ghat.begin() );
+        output.row(i) = ghat ;
     }
-    nn = k[c];
-    c = c-1;
-  }
-
-  /* free vector */
-  Free(k); 
-  Free(gew); 
-}
-}
-
-extern "C" {
-  SEXP misoreg ( SEXP data, SEXP weights );
-}
-
-SEXP misoreg ( SEXP data, SEXP weights ) {
-SEXP rl=R_NilValue;
-RcppMatrix<double> input(data);
-RcppVector<double> w(weights);
-RcppMatrix<double> output(data);
-int n,m;
-m= input.getDim2();
-n= input.getDim1();
-std::vector<double> stlwe(w.stlVector());
-std::vector< std::vector<double> > stlin(input.stlMatrix());
-std::vector< std::vector<double> > stlo(output.stlMatrix());
-for(int i=0; i<n; i++){
-C_isomean(&(stlin[i][0]),&stlwe[0],&m,&(stlo[i][0]));
-}
-RcppResultSet rs;
-rs.add("result",stlo);
-rs.add("weights",stlwe);
-rl = rs.getReturnList();
-return(rl);
-  Rf_warning("your C program does not return anything!");
-  return R_NilValue;
+    return List::create( 
+        _["result"] = output, 
+        _["weights"] = w
+        ) ;
+    
+END_RCPP
 }
 
